bea4865bfb3b5fe4e4051574e049745bd38e4955
DBJ <dbjdbj@gmail.com>
2010-02-17 18:11:37 +0000

    not using jQuery load Script which it seems made the loader work in IE, FF and
    WebKit ...

--- dbj.ldr.js
+++ dbj.ldr.js
@@ -25,21 +25,21 @@ this says that first file loaded OK, but second did not
 /// Dependencies : jQuery 1.3.2 or higher
 (function(global, undefined) {
 
-    if (global.dbj_loader_cache === undefined) {
-        global.dbj_loader_cache = {};
+    if (window.dbj_loader_cache === undefined) {
+        window.dbj_loader_cache = {};
     }
 
     var join = function() { return [].join.call(arguments, ''); },
 
     // we do this log-method-quickie here so that we do not depend on some library
     // if firebug or other window.console is not present
-    log_ = (!global.console || !global.console.log) ? function() {
+    log_ = (!console || !console.log) ? function() {
         document.body.innerHTML += ("<ul style='margin:2px; padding:2px; font:8px/1.0 verdana,tahoma,arial; color:black; background:white;'><li>" + [].join.call(arguments, '') + "</ul></li>").replace(/\n/g, "<br/>");
     } : function() {
-        global.console.log([].join.call(arguments, ''));
+        console.log([].join.call(arguments, ''));
     },
-    terror = function() { var s_ = [].join.call(arguments, ''); log_(s_); throw new Error(0xFFFF, "DBJ*Loader ERROR: " + s_); },
-        dbj = global.dbj || (global.dbj = {}),
+    terror = function() { var s_ = [].join.call(arguments, ''); log_.call(window, s_); throw new Error(0xFFFF, "DBJ*Loader ERROR: " + s_); },
+        dbj = dbj || (dbj = {}),
         STR_JQUERY_URL = "http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.min.js",
         STR_APPLY = "apply",
         STR_ASYNC = "async",
@@ -73,12 +73,13 @@ this says that first file loaded OK, but second did not
             try {
                 func[STR_APPLY](self || global, slice[STR_CALL](arguments, 2));
             } catch (x) {
-                terror(" name: ", x, "", ", message: ", x.message);
+                terror.call(global, " name: ", x, "", ", message: ", x.message);
             }
         }, time_out || 0);
     }
 
-    dbj.loadScript = function(options, callback) {
+    // for the time being this crazzy nugget is winning ...
+    var loadScript = function(options, callback) {
 
         if (global.dbj_loader_cache[options[STR_URL]]) return;
 
@@ -102,17 +103,16 @@ this says that first file loaded OK, but second did not
                 script[STR_ON_LOAD] = script[STR_ON_READY_STATE_CHANGE] = null;
 
                 global.dbj_loader_cache[this.src] = true;
-                log_(script.src, " Loaded");
+                log_.call(global, script.src, " Loaded");
 
                 head.removeChild(script);
 
-                if ("function" === typeof callback)
-                // {
-                //  if (global.opera)
-                    delayed_call(callback);
-                //else
-                //  callback();
-                //}
+                if ("function" === typeof callback) {
+                    if (global.opera)
+                        delayed_call(callback, global);
+                    else
+                        callback.call(global);
+                }
             }
         };
         // Use insertBefore instead of appendChild  to circumvent an IE6 bug.
@@ -122,83 +122,92 @@ this says that first file loaded OK, but second did not
 
     //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    function keys(object) {
+        var k = []; for (var name in object) {
+            if (Object.prototype.hasOwnProperty.call(object, name))
+                k.push(name);
+        }
+        return k;
+    }
+
     var loader = function(jQuery, CFG_PATH, CFG_FILE, callback, undefined) {
         jQuery.ajaxSetup({ async: false }); // CRUCIAL!
         $.getJSON(
         CFG_PATH + CFG_FILE,
         function(data, stat) {
-            // data will be a jsonObj
-            // stat will be one of the following values: 
-            // "timeout","error","notmodified","success","parsererror"
-            // this; is the options for this ajax request
-            for (var js in data) {
-                if (global.dbj_loader_cache[CFG_PATH + js]) continue;
-                $.getScript(CFG_PATH + js, function(data, stat) {
-                    // save the status for this file
-                    global.dbj_loader_cache[CFG_PATH + js] = stat;
+            // data will be a jsonObj, stat will be one of the following values:
+            // "timeout","error","notmodified","success","parsererror", this is the options for this ajax request
+            var key = keys(data);
+            function inner_loader(j,key) {
+                var js = key[j];
+                if (!js) return;
+                loadScript({ "url": CFG_PATH + js }, function() {
                     log_("Loaded:", CFG_PATH, js, " :status: ", stat);
+                    if (j === ( key.length -1 ) ) {
+                        log_("j: ", j, ", l: ", key.length-1, ", time to call the final callback");
+                        if ("function" === typeof callback) {
+                            log_("Now calling ONREADY function: ", callback+"");
+                            delayed_call(callback, window);
+                            return;
+                        }
+                    } else {
+                        inner_loader(j+1, key );
+                    }
                 });
             }
-            log_("DONE, status: ", stat);
-            if ("function" === typeof callback) {
-                log_("Will be calling onready handler function");
-                $(callback);
-            }
+            inner_loader(0, key);
         });
     };
     //
     var on_jq_ready = function() {
-        jQuery(function($) {
-                //
-                jQuery(document.body).error(function(msg, url, line) {
-                    log_("DBJ*Loader XHR Error: ", msg, "url: ", url, line); return false;
-                });
-                jQuery(document.body).ajaxError(function(event, xhr, settings, thrownError) {
-                    log_("DBJ*Loader XHR Error requesting: ", settings.url, (thrownError ? ", message: " + thrownError.message : "")); return false;
-                });
-
-                var $cfg = jQuery("script[" + STR_CFG_ATT + "][src]");
-                if ($cfg.length < 1) {
-                    terror("At least one script element must have valid both src and ", STR_CFG_ATT, " attributes");
-                }
-
-                $cfg.each(function() {
-                try {
-                    var $this = jQuery(this),
+        /*
+        jQuery(document.body).error(function(msg, url, line) {
+        log_("DBJ*Loader XHR Error: ", msg, "url: ", url, line); return false;
+        });
+        jQuery(document.body).ajaxError(function(event, xhr, settings, thrownError) {
+        log_("DBJ*Loader XHR Error requesting: ", settings.url, (thrownError ? ", message: " + thrownError.message : "")); return false;
+        });
+        */
+        var $cfg = jQuery("script[" + STR_CFG_ATT + "][src]");
+        if ($cfg.length < 1) {
+            terror.call(global, "At least one script element must have valid both src and ", STR_CFG_ATT, " attributes");
+        }
+        $cfg.each(function() {
+            try {
+                var $this = jQuery(this),
                         CFG_FILE = $this.attr(STR_CFG_ATT),
                         CFG_PATH = $this.attr(STR_PTH_ATT), // try to use path attribute from the script element
                         path = $this.attr('src');
-                    if (undefined === CFG_FILE)
-                        terror(STR_CFG_ATT + " attribute is not defined?");
-                    if (CFG_PATH === undefined) {
-                        // make path to be the same as script src attribute path component
-                        CFG_PATH = path.match(/^.*\//) ? "" + path.replace(/\\/g, "/").match(/^.*\//) : "./";
-                    }
-                    var defualt_onready = new Function(join(" console.log('no ready handler defined for:", CFG_PATH, CFG_FILE, "')")),
+                if (undefined === CFG_FILE)
+                    terror.call(global, STR_CFG_ATT + " attribute is not defined?");
+                if (CFG_PATH === undefined) {
+                    // make path to be the same as script src attribute path component
+                    CFG_PATH = path.match(/^.*\//) ? "" + path.replace(/\\/g, "/").match(/^.*\//) : "./";
+                }
+                var defualt_onready = new Function(join(" console.log('no ready handler defined for:", CFG_PATH, CFG_FILE, "')")),
                         on_ready;
-                    try {
-                        on_ready = (new Function("return " + $this.attr(STR_CFG_READY)))();
+                try {
+                    on_ready = (new Function("return " + $this.attr(STR_CFG_READY)))();
 
-                        if ("function" !== typeof on_ready) {
-                            log_("User defined ready handler can not be used, because it is not a function");
-                            on_ready = defualt_onready;
-                        }
-                    } catch (x) {
-                        log_("ERROR while evaluating _ON_READY_ attribute. Default onready handler will be used.");
+                    if ("function" !== typeof on_ready) {
+                        log_.call(global, "User defined ready handler can not be used, because it is not a function");
                         on_ready = defualt_onready;
                     }
-                        loader(jQuery, CFG_PATH, CFG_FILE, on_ready);
-                    } catch (x) {
-                        log_("ERRROR:\n" + x.message);
-                    }
-                });
+                } catch (x) {
+                    log_.call(global, "ERROR while evaluating _ON_READY_ attribute. Default onready handler will be used.");
+                    on_ready = defualt_onready;
+                }
+                loader(jQuery, CFG_PATH, CFG_FILE, on_ready);
+            } catch (x) {
+                log_.call(global, "ERRROR:\n" + x.message);
+            }
         });
     };
     //
     top.dbj_was_here = false;
     if (false === top.dbj_was_here) {
         top.dbj_was_here = true;
-        dbj.loadScript({ "url": STR_JQUERY_URL }, on_jq_ready);
+        loadScript({ "url": STR_JQUERY_URL }, on_jq_ready);
     }
 })(window);
     ////////////////////////////////////////////////////////////////////////////////////////
