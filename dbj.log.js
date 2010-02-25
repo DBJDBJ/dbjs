4e2aff909599a1582b438770be331b0cc656327c
DBJ <dbjdbj@gmail.com>
2010-02-25 14:34:24 +0000

    inclusion of FireBug Lite, for IE , moved to dbj.log.js

--- dbj.log.js
+++ dbj.log.js
@@ -1,33 +1,24 @@
-﻿/// <reference path="../../jq132-vsdoc.js" />
+﻿
 ///
 /// GPL (c) 2009 by DBJ.ORG
 /// DBJ.LOG.JS(tm)
 ///
 /// $Revision: 1 $$Date: 11/01/10 16:00 $
 ///
-/// Dependencies : jQuery 1.3.2 or higher
-(function($, dbj) {
+/// depends on : http://dbj.org/4/fblight
 
-//TODO: find some jquery logging pluging... for IE
-dbj.log = function() {
-    if (!top.window.console || !top.window.console.log ) {
-        $(document.body).prepend(("<ul style='font:8px/1.5 verdana,tahoma,arial; color:black; background:white;'><li>" + [].join.call(arguments, '') + "</ul></li>").replace(/\n/g, "<br/>"));
-    } else {
-        top.window.console.log([].join.call(arguments, ''));
-    }
-}
+document.write(
+"<!---- for IE use FireBug Light ----->" + 
+"<!--[if IE]>" + 
+'<link href="http://dbj.org/4/fblight/fbl.css" rel="stylesheet" type="text/css" />' + 
+'<script src="http://dbj.org/4/fblight/fbl.js" type="text/javascript"><'+'/script>' + 
+'<script type="text/javascript" >' +
+"   onload = function () { var tid = setTimeout( function () { if (window.firebug) {" + 
+"        clearTimeout(tid); delete tid; " +
+"        firebug.env.debug = false;  /*open minimized*/" + 
+"        firebug.env.detectFirebug;  /*do not initialize if Firebug is active */" + 
+"    }}, 1000 ); }" + 
+" <"+"/script>" + 
+"<![endif]-->"
+);
 
-})(jQuery, top.dbj);
-
-/*
-Or just use this bellow!
-
-<!--[if IE]>
-<p>Firebug&trade;Lite : Click on it or drag and drop the link bellow to the Links Toolbar</p>
-<p>
-<a href="javascript:var%20firebug=document.createElement('script');firebug.setAttribute('src','http://dbj.org/4/fblight/fbl.js');document.body.appendChild(firebug);(function(){if(window.firebug.version){firebug.init();}else{setTimeout(arguments.callee);}})();void(firebug);">
-<img src="http://themeforest.s3.amazonaws.com/45_firefox/firebug.jpg" alt="Firebug Lite" title="Click for Firebug Lite. Or drag and drop this link onto the links toolbar." style="width:48px;height:48px;border-width:0px;" />
-</a>
-</p>
-<![endif]-->
-*/
